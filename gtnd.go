f6feca2268e1d8a149fb47f5f06cf25a5c32472f
dmnlk <seikima2demon@gmail.com>
2015-01-14 01:32:57 +0900

    implements Search method

--- gtnd.go
+++ gtnd.go
@@ -1,6 +1,34 @@
 package gtnd
 
-type Gtnd struct {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
 
+const (
+	URL = "http://api.atnd.org/events/"
+)
+
+func Search(keyword string) (*SearchResult, error) {
+	param := url.Values{}
+	param.Add("keyword", keyword)
+	param.Add("format", "json")
+
+	resp, err := http.Get(URL + "?" + param.Encode())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	val, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result *SearchResult
+	if err = json.Unmarshal(val, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
 }
-
